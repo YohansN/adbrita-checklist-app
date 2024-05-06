7bdd7b460b5a0f1498a78cc85e7f1be3054e233d
Yohans Nascimento <yohans.dev@gmail.com>
2024-05-06 19:25:40 -0300

    adjust: added prop for save input value when re-rendering

--- src/components/FormItemNote/index.tsx
+++ src/components/FormItemNote/index.tsx
@@ -3,13 +3,19 @@ import {styles} from './styles';
 
 interface FormItemNoteProps{
     onChangeText: (value: string) => void;
+    defaultValue?: string;
 }
 
-export default function FormItemNote({onChangeText}: FormItemNoteProps){
+export default function FormItemNote({onChangeText, defaultValue}: FormItemNoteProps){
     return(
         <View style={styles.container}>
             <Text style={styles.text}>Observação:</Text>
-            <TextInput style={styles.input} placeholder="Adicione uma observação se necessário" onChangeText={onChangeText}/>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Adicione uma observação se necessário" 
+                onChangeText={onChangeText}
+                defaultValue={defaultValue}
+            />
         </View>
     )
 }
